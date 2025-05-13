552bb02e1db5a14df038ec627defc3dc52d602fa
MlgDev <julien.malige@hotmail.fr>
2025-05-14 01:57:57 +0200

    fix header mobile

--- client/src/app/shared/header/header.component.ts
+++ client/src/app/shared/header/header.component.ts
@@ -1,10 +1,12 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  imports: [RouterModule]
+  imports: [CommonModule, RouterModule]
 })
 export class HeaderComponent {
+  isMenuOpen = false;
 }
