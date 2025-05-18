d44d6cd51876096cbbac319d37660d51cf7bf811
MlgDev <julien.malige@hotmail.fr>
2025-05-18 20:15:29 +0200

    fix bug on register

--- api/app/routes/auth_routes.py
+++ api/app/routes/auth_routes.py
@@ -17,8 +17,13 @@ serializer = itsdangerous.URLSafeTimedSerializer('secret-reset-token')
 @auth_bp.route('/register', methods=['POST'])
 def register():
     data = request.get_json()
+    # Trimer les espaces en début et en fin pour chaque champ
+    data = {key: value.strip() if isinstance(value, str) else value for key, value in data.items()}
+
+    if not data.get('username') or not data.get('email') or not data.get('password'):
+        return jsonify({"message": "Missing fields"}), 400
     if User.query.filter_by(email=data['email']).first():
-        return jsonify({"msg": "Email already used"}), 400
+        return jsonify({"message": "Email already used"}), 400
     hashed = hash_password(data['password'])
     user = User(username=data['username'], email=data['email'], password_hash=hashed)
     db.session.add(user)
