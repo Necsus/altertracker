21dba26ed48cc75f57a014e4b1f15215fdab9d65
MlgDev <julien.malige@hotmail.fr>
2025-05-19 20:16:43 +0200

    debut toggle mail

--- client/src/app/user/user-alerts/user-alerts.component.ts
+++ client/src/app/user/user-alerts/user-alerts.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 import { catchError, from, map, mergeMap, of, throwError } from 'rxjs';
 import { OfferLiveMarketRequest } from '../../01_models/02_api/card/offer-live-market-request.model';
@@ -15,7 +16,7 @@ import { ToastService } from '../../shared/services/toast/toast.service';
   selector: 'app-user-alerts',
   templateUrl: './user-alerts.component.html',
   styleUrls: ['./user-alerts.component.css'],
-  imports: [CommonModule, CardComponent]
+  imports: [CommonModule, FormsModule, CardComponent]
 })
 export class UserAlertsComponent implements OnInit {
   isLoading: boolean = false; // État de chargement
@@ -86,6 +87,15 @@ export class UserAlertsComponent implements OnInit {
               }
             });
         }
+      }
+    });
+  }
+
+  onToggleNotification(alert: UserAlertModel): void {
+    // Inverse l'état de notification
+    this.userService.put_user_alert$(alert).subscribe({
+      next: () => {
+        alert.mail_active = !alert.mail_active;
       }
     });
   }
