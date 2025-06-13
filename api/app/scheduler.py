1f5ab1637f9c02bdeb418b9a2168aaa21d07f25a
MlgDev <julien.malige@hotmail.fr>
2025-06-14 01:24:25 +0200

    appscheduler

---
+++ api/app/scheduler.py
@@ -0,0 +1,24 @@
+from apscheduler.schedulers.background import BackgroundScheduler
+from apscheduler.triggers.cron import CronTrigger
+from app.routes.script_routes import dispatch_script
+from app import create_app
+
+def start_scheduler():
+    app = create_app()
+    scheduler = BackgroundScheduler(timezone="Europe/Paris")
+
+    # Script toutes les 24h
+    scheduler.add_job(
+        lambda: run_script_with_context(app, 'script_get_unique'),
+        trigger=CronTrigger(hour=2, minute=0))
+
+    # Script toutes les 4h
+    scheduler.add_job(
+        lambda: run_script_with_context(app, 'script_get_offers'), 
+        trigger=CronTrigger(hour='0,4,8,12,16,20', minute=0))
+
+    scheduler.start()
+
+def run_script_with_context(app, script_name):
+    with app.app_context():
+        dispatch_script(script_name)
--- api/run.py
+++ api/run.py
@@ -2,8 +2,10 @@ import eventlet
 eventlet.monkey_patch()
 from app import create_app
 from app.extensions import socketio
+from app.scheduler import start_scheduler
 
 app = create_app()
+start_scheduler()
 
 if __name__ == '__main__':
     socketio.run(app, debug=True, host='0.0.0.0', port=5000)
