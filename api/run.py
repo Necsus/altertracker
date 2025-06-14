481853487c9cc8f14fd378f803a296eb027ad339
MlgDev <julien.malige@hotmail.fr>
2025-06-14 11:20:51 +0200

    Update run.py

--- api/run.py
+++ api/run.py
@@ -3,9 +3,11 @@ eventlet.monkey_patch()
 from app import create_app
 from app.extensions import socketio
 from app.scheduler import start_scheduler
+from app.config import ConfigEnv
 
 app = create_app()
-start_scheduler(app)
+if ConfigEnv.FLASK_ENV == 'production':
+    start_scheduler(app)
 
 if __name__ == '__main__':
     socketio.run(app, debug=True, host='0.0.0.0', port=5000)
