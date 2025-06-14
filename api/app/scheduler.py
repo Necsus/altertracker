473e4fb8d7f2c36e194fc7d42d09c906906f0460
MlgDev <julien.malige@hotmail.fr>
2025-06-14 16:58:55 +0200

    Update scheduler.py

--- api/app/scheduler.py
+++ api/app/scheduler.py
@@ -12,12 +12,12 @@ def start_scheduler(app):
     # Script toutes les 24h
     scheduler.add_job(
         lambda: run_script_with_context(app, 'script_get_unique'),
-        trigger=CronTrigger(hour=4, minute=0))
+        trigger=CronTrigger(hour='4,16', minute=0))
 
     # Script toutes les 4h
     scheduler.add_job(
         lambda: run_script_with_context(app, 'script_get_offers'),
-        trigger=CronTrigger(hour='2,6,10,14,18,22', minute=0))
+        trigger=CronTrigger(hour='2,8,14,20', minute=0))
 
     scheduler.start()
 
