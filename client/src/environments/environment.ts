22460be851a0a03b25408bf5a650e96fa2f17321
MlgDev <julien.malige@hotmail.fr>
2025-06-17 19:21:53 +0200

    conf discord

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
-  api_url: 'http://127.0.0.1:5001/api',
-  socketio_url: 'http://127.0.0.1:5001',
-  discord_client_id: '',
-  discord_redirect_uri: 'http://'
+  api_url: 'http://127.0.0.1:5000/api',
+  socketio_url: 'http://127.0.0.1:5000',
+  discord_client_id: '1379912870745346199',
+  discord_redirect_uri: 'http://localhost:4200/me/discord/link'
 }
