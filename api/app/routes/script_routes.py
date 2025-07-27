5fbb201cbf7e210b6051adf313989daef6ac0cb6
MlgDev <julien.malige@hotmail.fr>
2025-07-27 09:14:31 +0200

    Update script_routes.py

--- api/app/routes/script_routes.py
+++ api/app/routes/script_routes.py
@@ -5,7 +5,13 @@ from flask_socketio import emit
 from app.extensions import socketio, limiter
 from app.decorators.auth_decorator import admin_required
 from app.scripts import (
-  script_get_no_unique, script_get_unique, script_get_en, script_get_offers, script_reload_main_effect, script_get_offer_currency
+  script_get_no_unique,
+  script_get_unique,
+  script_get_en,
+  script_get_offers,
+  script_reload_main_effect,
+  script_get_offer_currency,
+  script_user_search_notify
 )
 
 script_bp = Blueprint('script', __name__)
@@ -16,7 +22,8 @@ def dispatch_script(script: str, workers: int = 3, faction: str = None):
         script_get_unique.run_script(faction, workers, True)
         time.sleep(5)
         script_get_en.run_script(faction, 1)
-
+        # time.sleep(5)
+        # script_user_search_notify.run_script(1)
     if script == 'script_get_no_unique':
         script_get_no_unique.run_script()
         time.sleep(5)
