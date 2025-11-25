39147dc24a08839209c9096850c91e01c19b77a6
MlgDev <julien.malige@hotmail.fr>
2025-11-25 19:29:33 +0100

    end

---
+++ client/src/app/end-of-service/end-of-service.component.ts
@@ -0,0 +1,11 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-end-of-service',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './end-of-service.component.html'
+})
+export class EndOfServiceComponent {
+}
